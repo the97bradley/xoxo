cf1f68a0fe03dd2ba13d13c0ae3d1bba1134c05e
veryspry <ehlinger.matt@gmail.com>
2018-07-05 13:28:43 -0500

    initial

--- game/index.js
+++ game/index.js
@@ -1,6 +1,20 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const board = Map()
+
+const MOVE = 'MOVE'
+const START = 'START'
+const move = (turn, pos) => ({type: MOVE, turn: turn, position: pos})
+
+export default function gameReducer(state = {board: board, turn: 'X'}, action) {
+  switch (action.type) {
+    case MOVE:
+      return {board: state.board.setIn(action.position, action.turn), turn: action.turn}
+    // case START:
+    //   return state
+    default:
+      return state
+  }
 }
+
+export {board, move}
